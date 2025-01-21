64eef18d975652ae6082f9480ca6bfccb97cf858
SkyH1ghDev <95106202+SkyH1ghDev@users.noreply.github.com>
2025-01-21 09:19:22 +0100

    Fixed main-function and added memory-leak checking

--- Application/src/main.cpp
+++ Application/src/main.cpp
@@ -1,9 +1,24 @@
-#include <iostream>
+#define _CRTDBG_MAP_ALLOC
+#define WIN32_LEAN_AND_MEAN
 
-#include <lib.h>
+#include <crtdbg.h>
+#include <Windows.h>
 
-int main()
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                      _In_opt_ HINSTANCE hPrevInstance,
+                      _In_ LPWSTR    lpCmdLine,
+                      _In_ int       nCmdShow)
 {
-    std::cout << "Hello world!\n" << "The meaning of life is " << DummyLibNamespace::libFunc() << "\n";
-    return 0;
+
+	// Braces to let the application go out of scope before checking memory leaks
+	{
+		
+		// Have "Application"-object that gets allocated on stack
+		
+	}
+
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+	_CrtDumpMemoryLeaks();
+
+	return 0;
 }
