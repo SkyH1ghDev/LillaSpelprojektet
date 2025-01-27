9b6a7e0d130b65adc994c256ac5977ab6c7fd801
LioTB0 <lio.ballantyne@gmail.com>
2025-01-27 12:37:51 +0100

    Update premake5.lua

--- Tests/premake5.lua
+++ Tests/premake5.lua
@@ -3,9 +3,12 @@ project "Tests"
     targetdir(targetBuildPath .. "/%{prj.name}")
     objdir(objBuildPath .. "/%{prj.name}")
     files {"src/**"}
-    includedirs{"../Libraries/include/**", "../Libraries/include/", targetBuildPath .. "/External/include"}
-
-    libdirs{targetBuildPath .. "/External/lib"}
+    includedirs{"../Libraries/include/**", 
+        "../Libraries/include/", 
+        "../External/googletest/googletest/include/",
+        "../Application/src/",
+        "../Application/src/**"}
+    libdirs{targetBuildPath .. "/External/lib/"}
 
     -- Although this consoleApp does not link to the "googletest" project per say, it is still dependant on its build to finish
     dependson{"googletest"}
