129a80bd215e7b6129e6a8078db72719bbc9335f
Shoruda <123826451+Shoruda@users.noreply.github.com>
2025-01-28 09:38:25 +0100

    changed strnig to enum and if to swich case

--- Application/src/GameObjects/Entity/Entity.cpp
+++ Application/src/GameObjects/Entity/Entity.cpp
@@ -1,69 +1,59 @@
 #include "Entity.hpp"
-#include "PlayerAttack.hpp"
-#include "PlayerMove.hpp"
-#include "PlayerTakeDamage.hpp"
-#include "PlayerUseCard.hpp"
-#include "PlayerVisible.hpp"
-#include "EnemyAttack.hpp"
-#include "EnemyMove.hpp"
-#include "EnemyTakeDamage.hpp"
-#include "EnemyUseCard.hpp"
-#include "EnemyVisible.hpp"
 #include <iostream>
 
 // Factory function to create appropriate components
-std::unique_ptr<IAttack> createAttackComponent(const std::string& type) {
-    if (type == "player") {
+std::unique_ptr<IAttack> createAttackComponent(EntityType type) {
+    switch (type) {
+    case EntityType::Player:
         return std::make_unique<PlayerAttack>();
-    }
-    else if (type == "enemy") {
+    case EntityType::Enemy:
         return std::make_unique<EnemyAttack>();
     }
     return nullptr;
 }
 
-std::unique_ptr<IMove> createMoveComponent(const std::string& type) {
-    if (type == "player") {
+std::unique_ptr<IMove> createMoveComponent(EntityType type) {
+    switch (type) {
+    case EntityType::Player:
         return std::make_unique<PlayerMove>();
-    }
-    else if (type == "enemy") {
+    case EntityType::Enemy:
         return std::make_unique<EnemyMove>();
     }
     return nullptr;
 }
 
-std::unique_ptr<ITakeDamage> createTakeDamageComponent(const std::string& type) {
-    if (type == "player") {
+std::unique_ptr<ITakeDamage> createTakeDamageComponent(EntityType type) {
+    switch (type) {
+    case EntityType::Player:
         return std::make_unique<PlayerTakeDamage>();
-    }
-    else if (type == "enemy") {
+    case EntityType::Enemy:
         return std::make_unique<EnemyTakeDamage>();
     }
     return nullptr;
 }
 
-std::unique_ptr<IUseCard> createUseCardComponent(const std::string& type) {
-    if (type == "player") {
+std::unique_ptr<IUseCard> createUseCardComponent(EntityType type) {
+    switch (type) {
+    case EntityType::Player:
         return std::make_unique<PlayerUseCard>();
-    }
-    else if (type == "enemy") {
+    case EntityType::Enemy:
         return std::make_unique<EnemyUseCard>();
     }
     return nullptr;
 }
 
-std::unique_ptr<IVisible> createVisibleComponent(const std::string& type) {
-    if (type == "player") {
+std::unique_ptr<IVisible> createVisibleComponent(EntityType type) {
+    switch (type) {
+    case EntityType::Player:
         return std::make_unique<PlayerVisible>();
-    }
-    else if (type == "enemy") {
+    case EntityType::Enemy:
         return std::make_unique<EnemyVisible>();
     }
     return nullptr;
 }
 
 // Constructor: Decides behavior based on entity type
-Entity::Entity(const std::string& entityType)
+Entity::Entity(EntityType entityType)
     : type(entityType),
     move(createMoveComponent(entityType)),
     attack(createAttackComponent(entityType)),
@@ -71,5 +61,5 @@ Entity::Entity(const std::string& entityType)
     useCard(createUseCardComponent(entityType)),
     visible(createVisibleComponent(entityType))
 {
-    std::cerr << "Entity created of type: " << entityType << std::endl;
+    std::cerr << "Entity created of type: " << (type == EntityType::Player ? "Player" : "Enemy") << std::endl;
 }
--- Application/src/GameObjects/Entity/Entity.hpp
+++ Application/src/GameObjects/Entity/Entity.hpp
@@ -6,27 +6,44 @@
 #include "IVisible.hpp"
 #include "ITakeDamage.hpp"
 #include "IUseCard.hpp"
-#
+#include "IGameObject.hpp"
+
+#include "PlayerAttack.hpp"
+#include "PlayerMove.hpp"
+#include "PlayerTakeDamage.hpp"
+#include "PlayerUseCard.hpp"
+#include "PlayerVisible.hpp"
+
+#include "EnemyAttack.hpp"
+#include "EnemyMove.hpp"
+#include "EnemyTakeDamage.hpp"
+#include "EnemyUseCard.hpp"
+#include "EnemyVisible.hpp"
+
+// Define an enum for the entity type
+enum class EntityType {
+    Player,
+    Enemy
+};
+
 class Entity
 {
 private:
-	std::unique_ptr<IMove> move;
-	std::unique_ptr<IVisible> visible;
-	std::unique_ptr<IAttack> attack;
-	std::unique_ptr<ITakeDamage> takeDamage;
-	std::unique_ptr<IUseCard> useCard;
-	std::string type;
+    std::unique_ptr<IMove> move;
+    std::unique_ptr<IVisible> visible;
+    std::unique_ptr<IAttack> attack;
+    std::unique_ptr<ITakeDamage> takeDamage;
+    std::unique_ptr<IUseCard> useCard;
+    EntityType type;
 
 public:
-	
-	Entity(const std::string& entityType);
+    Entity(EntityType entityType);
 
-	void performMove() { if (move) move->Move(); }
-	void performRender() { if (visible) visible->Visible(); }
-	void performAttack() { if (attack) attack->Attack(); }
-	void performTakeDamage() { if (takeDamage) takeDamage->TakeDamage(); }
-	void performUseCard() { if (useCard) useCard->UseCard(); }
+    void performMove() { if (move) move->Move(); }
+    void performVisible() { if (visible) visible->Visible(); }
+    void performAttack() { if (attack) attack->Attack(); }
+    void performTakeDamage() { if (takeDamage) takeDamage->TakeDamage(); }
+    void performUseCard() { if (useCard) useCard->UseCard(); }
 
-	std::string getType() const { return type; }
-
+    EntityType getType() const { return type; }
 };
