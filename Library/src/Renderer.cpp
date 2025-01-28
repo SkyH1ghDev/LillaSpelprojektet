70351be6d3b8bde8ba491dd5c262c50fe5630973
Ponlin02 <ponlin.02@gmail.com>
2025-01-28 16:40:04 +0100

    Changes to renderer
    Added more initializing declarations
    Used Attach

--- Library/src/Renderer.cpp
+++ Library/src/Renderer.cpp
@@ -5,9 +5,24 @@ Renderer::~Renderer()
 {
 }
 
+
+
+void Renderer::SetBlendState(MW::ComPtr<ID3D11DeviceContext>& immediateContext)
+{
+	float blendFactor[4] = {0.0f, 0.0f , 0.0f ,0.0f};
+	immediateContext.Get()->OMSetBlendState(m_blendState.Get(), blendFactor, 0xFFFFFFFF);
+}
+
+MW::ComPtr<ID3D11BlendState> Renderer::GetBlendState()
+{
+	return this->m_blendState;
+}
+
 void Renderer::InitializeBlendState(MW::ComPtr<ID3D11Device>& device)
 {
 	//Creating blendstate with default stuff
+	ID3D11BlendState* blendStateCpy;
+
 	D3D11_BLEND_DESC BlendState;
 	ZeroMemory(&BlendState, sizeof(BlendState));
 	BlendState.RenderTarget[0].BlendEnable = true;	//Enable blenging
@@ -19,20 +34,11 @@ void Renderer::InitializeBlendState(MW::ComPtr<ID3D11Device>& device)
 	BlendState.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
 	BlendState.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
 
-	HRESULT hr = device.Get()->CreateBlendState(&BlendState, this->r_blendState.GetAddressOf());
+	HRESULT hr = device.Get()->CreateBlendState(&BlendState, &blendStateCpy);
 	if (FAILED(hr))
 	{
 		throw std::runtime_error("Failed to create blend state!");
 	}
-}
 
-void Renderer::SetBlendState(MW::ComPtr<ID3D11DeviceContext>& immediateContext)
-{
-	float blendFactor[4] = {0.0f, 0.0f , 0.0f ,0.0f};
-	immediateContext.Get()->OMSetBlendState(r_blendState.Get(), blendFactor, 0xFFFFFFFF);
-}
-
-MW::ComPtr<ID3D11BlendState> Renderer::GetBlendState()
-{
-	return this->r_blendState;
+	this->m_blendState.Attach(blendStateCpy);
 }
