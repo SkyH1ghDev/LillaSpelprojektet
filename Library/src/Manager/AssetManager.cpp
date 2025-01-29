73d21b453c8d76b7be3d1bbccfedb23f791829f1
LioTB0 <lio.ballantyne@gmail.com>
2025-01-29 09:29:27 +0100

    Added file extension condition

--- Library/src/Manager/AssetManager.cpp
+++ Library/src/Manager/AssetManager.cpp
@@ -9,17 +9,22 @@ AssetManager::~AssetManager()
 {
 }
 
+//Reads all png and jpg files in folder specified by path string
 bool AssetManager::ReadFolder(MW::ComPtr<ID3D11Device> &device, std::string path)
 {
 	for (const auto& entry : FS::directory_iterator(path)) 
 	{
 		std::string filepath = entry.path().string();
-		filepath.at(filepath.find('\\')) = '/';
+		std::string extension = filepath.substr(filepath.find_last_of('.'), 4);
+		if (extension == ".jpg" || extension == ".png") 
+		{
+			filepath.at(filepath.find('\\')) = '/';
 
-		ShaderResourceTexture texture(device, filepath);
-		textures.push_back(texture);
+			ShaderResourceTexture texture(device, filepath);
+			textures.push_back(texture);
 
-		textureNames.push_back(filepath);
+			textureNames.push_back(filepath);
+		}
 	}
 
 	return true;
