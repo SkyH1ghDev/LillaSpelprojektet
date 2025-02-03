0b46402f4278344f093fb51ca06683cd6c8855e2
SkyH1ghDev <95106202+SkyH1ghDev@users.noreply.github.com>
2025-02-03 13:42:38 +0100

    Made Keyboard Static

--- Library/include/SpEngine/Input/Keyboard.hpp
+++ Library/include/SpEngine/Input/Keyboard.hpp
@@ -1,6 +1,6 @@
 ﻿#pragma once
 
-#include <map>
+#include <unordered_map>
 #include <Windows.h>
 
 #include "Key.hpp"
@@ -8,7 +8,7 @@
 class Keyboard
 {
 public:
-    Keyboard();
+    Keyboard() = default;
     ~Keyboard() = default;
     Keyboard(const Keyboard& other) = default;
     Keyboard& operator=(const Keyboard& other) = default;
@@ -18,7 +18,7 @@ public:
     /**
      * Handles the input from the keyboard
      */
-    void HandleInput();
+    static void HandleInput();
 
     /**
      * Gets key based on windows virtual key
@@ -26,7 +26,7 @@ public:
      * @param virtualKey Windows virtual key that represents what key object to return
      * @return Key object
      */
-    std::shared_ptr<Key> GetKey(const int& virtualKey);
+    static std::shared_ptr<Key> GetKey(const int& virtualKey);
 
 private:
 
@@ -35,14 +35,14 @@ private:
      *
      * @return Input message from the windows event system
      */
-    MSG ReadWindowsMessage();
+    static MSG ReadWindowsMessage();
 
 private:
-    std::map<int, std::shared_ptr<Key>> m_keys;
+    static std::unordered_map<int, std::shared_ptr<Key>> m_keys;
 
 };
 
 inline std::shared_ptr<Key> Keyboard::GetKey(const int& virtualKey)
 {
-    return this->m_keys[virtualKey];
+    return m_keys[virtualKey];
 }
