c46a83e4050ba0e2db0058356cfffcc528f209a0
Shoruda <123826451+Shoruda@users.noreply.github.com>
2025-02-04 11:02:15 +0100

    Update PlayerMove.cpp

--- Application/src/GameObjects/Entity/Player/PlayerMove.cpp
+++ Application/src/GameObjects/Entity/Player/PlayerMove.cpp
@@ -7,7 +7,7 @@ void PlayerMove::Move(DX::XMFLOAT2& m_position, DX::XMFLOAT2& m_direction) {
     DX::XMVECTOR direction = XMLoadFloat2(&m_direction);
 
     // Perform the movement operation
-    DX::XMVECTOR movement = DX::XMVectorScale(direction, 1.0f);
+    DX::XMVECTOR movement = DX::XMVectorScale(direction, 0.2f);
     position = DX::XMVectorAdd(position, movement);
 
     // Store the result back into m_position
