3df97bfcc1e0546fd67bbc84e6d7f09bc3b0dda5
Ponlin02 <ponlin.02@gmail.com>
2025-02-04 16:48:56 +0100

    Updated tests
    Setuptests

--- Tests/src/Test/SetupTest.cpp
+++ Tests/src/Test/SetupTest.cpp
@@ -1,15 +1,14 @@
 #pragma once
 #include <gtest/gtest.h>
-#include <Sprite.hpp>
 #include <SetupHelper.hpp>
+#include <Window.hpp>
 
 TEST(DirectX11, DeviceSetup)
 {
-    UINT height = 1080;
-    UINT width = 1920;
-
     HINSTANCE hInstance;
-    HWND window;
+    UINT width = 1920;
+    UINT height = 1080;
+    
     MW::ComPtr<ID3D11Device> device;
     MW::ComPtr<ID3D11DeviceContext> deviceContext;
     MW::ComPtr<IDXGISwapChain> swapChain;
@@ -17,19 +16,20 @@ TEST(DirectX11, DeviceSetup)
     MW::ComPtr<ID3D11DepthStencilView> dsView;
     MW::ComPtr<ID3D11RenderTargetView> rtv;
 
+    Window window = Window(hInstance, SW_SHOW, width, height);
+
     SetupHelper setup;
-    setup.Setup(hInstance, SW_SHOW, window, device, deviceContext, swapChain, texture, dsView, rtv, width, height);
+    setup.Setup(window.GetWindowHandle(), device, deviceContext, swapChain, texture, dsView, rtv, width, height);
 
     ASSERT_NE(device.Get(), nullptr);
 }
 
 TEST(DirectX11, ContextSetup)
 {
-    UINT height = 1080;
-    UINT width = 1920;
-
     HINSTANCE hInstance;
-    HWND window;
+    UINT width = 1920;
+    UINT height = 1080;
+
     MW::ComPtr<ID3D11Device> device;
     MW::ComPtr<ID3D11DeviceContext> deviceContext;
     MW::ComPtr<IDXGISwapChain> swapChain;
@@ -37,19 +37,20 @@ TEST(DirectX11, ContextSetup)
     MW::ComPtr<ID3D11DepthStencilView> dsView;
     MW::ComPtr<ID3D11RenderTargetView> rtv;
 
+    Window window = Window(hInstance, SW_SHOW, width, height);
+
     SetupHelper setup;
-    setup.Setup(hInstance, SW_SHOW, window, device, deviceContext, swapChain, texture, dsView, rtv, width, height);
+    setup.Setup(window.GetWindowHandle(), device, deviceContext, swapChain, texture, dsView, rtv, width, height);
 
     ASSERT_NE(deviceContext.Get(), nullptr);
 }
 
 TEST(DirectX11, SwapChainSetup)
 {
-    UINT height = 1080;
-    UINT width = 1920;
-
     HINSTANCE hInstance;
-    HWND window;
+    UINT width = 1920;
+    UINT height = 1080;
+
     MW::ComPtr<ID3D11Device> device;
     MW::ComPtr<ID3D11DeviceContext> deviceContext;
     MW::ComPtr<IDXGISwapChain> swapChain;
@@ -57,19 +58,20 @@ TEST(DirectX11, SwapChainSetup)
     MW::ComPtr<ID3D11DepthStencilView> dsView;
     MW::ComPtr<ID3D11RenderTargetView> rtv;
 
+    Window window = Window(hInstance, SW_SHOW, width, height);
+
     SetupHelper setup;
-    setup.Setup(hInstance, SW_SHOW, window, device, deviceContext, swapChain, texture, dsView, rtv, width, height);
+    setup.Setup(window.GetWindowHandle(), device, deviceContext, swapChain, texture, dsView, rtv, width, height);
 
     ASSERT_NE(swapChain.Get(), nullptr);
 }
 
 TEST(DirectX11, dsTextureSetup)
 {
-    UINT height = 1080;
-    UINT width = 1920;
-
     HINSTANCE hInstance;
-    HWND window;
+    UINT width = 1920;
+    UINT height = 1080;
+
     MW::ComPtr<ID3D11Device> device;
     MW::ComPtr<ID3D11DeviceContext> deviceContext;
     MW::ComPtr<IDXGISwapChain> swapChain;
@@ -77,19 +79,20 @@ TEST(DirectX11, dsTextureSetup)
     MW::ComPtr<ID3D11DepthStencilView> dsView;
     MW::ComPtr<ID3D11RenderTargetView> rtv;
 
+    Window window = Window(hInstance, SW_SHOW, width, height);
+
     SetupHelper setup;
-    setup.Setup(hInstance, SW_SHOW, window, device, deviceContext, swapChain, texture, dsView, rtv, width, height);
+    setup.Setup(window.GetWindowHandle(), device, deviceContext, swapChain, texture, dsView, rtv, width, height);
 
     ASSERT_NE(texture.Get(), nullptr);
 }
 
 TEST(DirectX11, dsViewSetup)
 {
-    UINT height = 1080;
-    UINT width = 1920;
-
     HINSTANCE hInstance;
-    HWND window;
+    UINT width = 1920;
+    UINT height = 1080;
+
     MW::ComPtr<ID3D11Device> device;
     MW::ComPtr<ID3D11DeviceContext> deviceContext;
     MW::ComPtr<IDXGISwapChain> swapChain;
@@ -97,19 +100,20 @@ TEST(DirectX11, dsViewSetup)
     MW::ComPtr<ID3D11DepthStencilView> dsView;
     MW::ComPtr<ID3D11RenderTargetView> rtv;
 
+    Window window = Window(hInstance, SW_SHOW, width, height);
+
     SetupHelper setup;
-    setup.Setup(hInstance, SW_SHOW, window, device, deviceContext, swapChain, texture, dsView, rtv, width, height);
+    setup.Setup(window.GetWindowHandle(), device, deviceContext, swapChain, texture, dsView, rtv, width, height);
 
     ASSERT_NE(dsView.Get(), nullptr);
 }
 
 TEST(DirectX11, rtvSetup)
 {
-    UINT height = 1080;
-    UINT width = 1920;
-
     HINSTANCE hInstance;
-    HWND window;
+    UINT width = 1920;
+    UINT height = 1080;
+
     MW::ComPtr<ID3D11Device> device;
     MW::ComPtr<ID3D11DeviceContext> deviceContext;
     MW::ComPtr<IDXGISwapChain> swapChain;
@@ -117,8 +121,10 @@ TEST(DirectX11, rtvSetup)
     MW::ComPtr<ID3D11DepthStencilView> dsView;
     MW::ComPtr<ID3D11RenderTargetView> rtv;
 
+    Window window = Window(hInstance, SW_SHOW, width, height);
+
     SetupHelper setup;
-    setup.Setup(hInstance, SW_SHOW, window, device, deviceContext, swapChain, texture, dsView, rtv, width, height);
+    setup.Setup(window.GetWindowHandle(), device, deviceContext, swapChain, texture, dsView, rtv, width, height);
 
     ASSERT_NE(rtv.Get(), nullptr);
 }
