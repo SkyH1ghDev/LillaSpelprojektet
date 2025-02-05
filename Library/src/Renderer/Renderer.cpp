47bb1f04b5c86ffcc8ef6e594a12bef39bc73e7c
Ponlin02 <ponlin.02@gmail.com>
2025-02-05 15:31:11 +0100

    Testing

--- Library/src/Renderer/Renderer.cpp
+++ Library/src/Renderer/Renderer.cpp
@@ -59,11 +59,14 @@ void Renderer::DrawScene(const std::shared_ptr<IScene>& sceneToRender)
 
 	this->FinalBindings();
 
+	RECT rect = { 0, 0, 640, 360 };
+
 	for (int i = 0; i < len; i++)
 	{
 		if (ObjectVec.at(i)->ShouldRender())
 		{
-			this->DrawTexture(this->m_assetMan.GetSprite(ObjectVec.at(i)->GetTextureString()).GetSRV().Get(), ObjectVec.at(i)->GetPosition(), DX::Colors::White);
+			//this->DrawTexture(this->m_assetMan.GetSprite(ObjectVec.at(i)->GetTextureString()).GetSRV().Get(), ObjectVec.at(i)->GetPosition(), DX::Colors::White);
+			this->DrawTexture(this->m_assetMan.GetSprite(ObjectVec.at(i)->GetTextureString()).GetSRV().Get(), ObjectVec.at(i)->GetPosition(), &rect, DX::Colors::White, 0.0f, DX::XMFLOAT2(0, 0), 1.0f, DX::DX11::SpriteEffects_None, 0.0f);
 		}
 	}
 
