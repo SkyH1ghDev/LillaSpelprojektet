d2051e18d256ed3b01be93da70c0c2bc5a0d65ff
TothOliver <olivertoth.in@gmail.com>
2025-02-11 16:49:08 +0100

    Added acceleration and friction

--- Application/src/GameObjects/Entity/Player/PlayerMove.cpp
+++ Application/src/GameObjects/Entity/Player/PlayerMove.cpp
@@ -9,7 +9,7 @@ DX::XMFLOAT2 PlayerMove::Move(const DX::XMFLOAT2& position, const DX::XMFLOAT2& 
 
     //In the middle of the Dash
     if (isDashing) {
-        
+
         DX::XMVECTOR dashMovement = DX::XMVectorScale(dashDirection, dashSpeed * deltaTime);
         positionXMVector = DX::XMVectorAdd(positionXMVector, dashMovement);
 
@@ -17,35 +17,59 @@ DX::XMFLOAT2 PlayerMove::Move(const DX::XMFLOAT2& position, const DX::XMFLOAT2& 
 
         if (dashTimer <= 0.0f) {
             isDashing = false;
-			dashCooldownTimer = dashCooldown;
+            dashCooldownTimer = dashCooldown;
         }
 
-	}
-	//Start the Dash
-	else if (dashInput && dashCooldownTimer <= 0) {
+    }
+    //Start the Dash
+    else if (dashInput && dashCooldownTimer <= 0) {
         isDashing = true;
         dashTimer = dashDuration;
 
         if (DX::XMVector2Length(directionXMVector).m128_f32[0] > 0.0f) {
             dashDirection = directionXMVector;
-			facingDirection = directionXMVector;
-		}
+            facingDirection = directionXMVector;
+        }
         else {
             dashDirection = facingDirection;
         }
 
     }
-	//Regular movement
+    //Regular movement
     else {
-		if (dashCooldownTimer > 0)
-			dashCooldownTimer -= deltaTime;
+        if (dashCooldownTimer > 0)
+            dashCooldownTimer -= deltaTime;
 
-        DX::XMVECTOR movement = DX::XMVectorScale(directionXMVector, 250.0f * Clock::GetDeltaTime());
-        positionXMVector = DX::XMVectorAdd(positionXMVector, movement);
-		
-        if(DirectX::XMVector2Length(directionXMVector).m128_f32[0] > 0.0f)
+        if (DirectX::XMVector2Length(directionXMVector).m128_f32[0] > 0.0f) {
+            // Apply acceleration when there's input
             facingDirection = directionXMVector;
+            velocity = DX::XMVectorAdd(velocity, DX::XMVectorScale(DX::XMVector2Normalize(directionXMVector), acceleration * deltaTime));
+
+            // Cap velocity to max speed
+            if (DirectX::XMVector2Length(velocity).m128_f32[0] > maxVelocity) {
+                velocity = DX::XMVectorScale(DX::XMVector2Normalize(velocity), maxVelocity);
+            }
+        }
+        else {
+            // Deceleration when no input
+            if (DirectX::XMVector2Length(velocity).m128_f32[0] > 0.01f) {
+                velocity = DX::XMVectorSubtract(velocity, DX::XMVectorScale(DX::XMVector2Normalize(velocity), deceleration * deltaTime));
+            }
+
+            // Clamp velocity to zero if it's very small
+            if (DirectX::XMVector2Length(velocity).m128_f32[0] < 0.01f) {
+                velocity = DX::XMVectorZero();
+            }
+        }
+
+        // Apply friction
+        velocity = DX::XMVectorScale(velocity, 1.0f - friction * deltaTime);
+
+        // Update position based on velocity
+        DX::XMVECTOR movement = DX::XMVectorScale(velocity, deltaTime);
+        positionXMVector = DX::XMVectorAdd(positionXMVector, movement);
     }
+
 
 
     DX::XMFLOAT2 newPosition;
--- Application/src/GameObjects/Entity/Player/PlayerMove.hpp
+++ Application/src/GameObjects/Entity/Player/PlayerMove.hpp
@@ -7,8 +7,14 @@ public:
     DX::XMFLOAT2 Move(const DX::XMFLOAT2& position, const DX::XMFLOAT2& direction, bool dashing) override;
 
 private:
-    float dashSpeed = 500.0f; 
-    float dashDuration = 0.2f;
+    DX::XMVECTOR velocity = { 0.0f, 0.0f };
+    float acceleration = 400.0f;
+	float deceleration = 600.0f;
+	float maxVelocity = 200.0f;
+    float friction = 0.3f;
+
+    float dashSpeed = 400.0f; 
+    float dashDuration = 0.1f;
     float dashCooldownTimer = 0.0f;
     float dashCooldown = 0.1f;
     float dashTimer = 0.0f;
