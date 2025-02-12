16b849a555b2ed6e828ca24afc311abf1ca69de5
GuptaWaltersson <huwa22@student.bth.se>
2025-02-12 09:10:30 +0100

    implemented minor responsibilities for CardManager
    små uppgifter som cardmanager har, den tar basically hand om kortleken, den kan ändra abilities och lägga in nya kort i leken.

--- Application/src/GameObjects/Card/CardManager.cpp
+++ Application/src/GameObjects/Card/CardManager.cpp
@@ -5,15 +5,38 @@
 
 void CardManager::AddCard(std::shared_ptr<Card> Card)
 {
-
+    m_CardDeck.push(Card);
 }
 
 
-void CardManager::ToggleAbilitie()
+void CardManager::ChangeAbilitie()
 {
+    if (m_CardDeck.empty())
+    {
+        std::cout << "No abilities available" << std::endl;
+        return;
+    }
+
+    std::shared_ptr<Card> card = m_CardDeck.front();
+    m_CardDeck.pop();
+
+    //Here we can also if we want cast the "card" abilitie before putting it last
+
+    m_CardDeck.push(card);
 }
 
 bool CardManager::HasCard() const
 {
-    return false;
+    return !m_CardDeck.empty();
+}
+
+std::shared_ptr<Card> CardManager::GetTopCard()
+{
+    if (m_CardDeck.empty())
+    {
+        std::cerr << "Deck contains no card" << std::endl;
+        return;
+    }
+    std::shared_ptr<Card> TopCard = m_CardDeck.front();
+    return TopCard;
 }
--- Application/src/GameObjects/Card/CardManager.hpp
+++ Application/src/GameObjects/Card/CardManager.hpp
@@ -12,9 +12,25 @@ private:
     
     std::queue<std::shared_ptr<Card>> m_CardDeck;
 public:
+    /*
+    * Function that adds a card to your deck
+    */
     void AddCard(std::shared_ptr<Card> Card);
-    void ToggleAbilitie();
+
+    /*
+    * Call function after using a card, the top card goes to the bottom of the deck
+    */
+    void ChangeAbilitie();
+
+    /*
+    * Returns false if deck is empty, otherwise true
+    */
     bool HasCard() const;
+
+    /*
+    * Retreives the top card of deck
+    */
+    std::shared_ptr<Card> GetTopCard();
 };
 
 
