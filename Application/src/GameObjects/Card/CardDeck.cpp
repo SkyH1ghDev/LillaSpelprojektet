c29d207c98a565061f97a8acedefb7f2ba9a7197
GuptaWaltersson <huwa22@student.bth.se>
2025-02-13 08:40:23 +0100

    Carddeck filer

---
+++ Application/src/GameObjects/Card/CardDeck.cpp
@@ -0,0 +1,30 @@
+#include "Card.hpp"
+#include "CardActivatedComponentFactory.hpp"
+#include "CardAvailabilityComponentFactory.hpp"
+#include "CardVisibleComponentFactory.hpp"
+#include <iostream>
+
+
+
+Card::Card(CardType cardType) :
+    m_activated (CreateActivatedComponent(cardType)),
+    m_visible (CreateVisibleComponent(cardType)),
+    m_available (CreateAvailabilityComponent(cardType))
+{
+    std::cout << "Card created of type: " << (m_type == CardType::Shotgun ? "Shotgun" : "Shield") << "\n"; //implement switch case när vi har fler kort
+}
+
+void Card::OnStart()
+{
+
+}
+
+CardType Card::GetType()
+{
+    return m_type;
+}
+
+void Card::Update()
+{
+
+}
---
+++ Application/src/GameObjects/Card/CardDeck.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include <memory>
+
+#include <SpEngine/Assets/IGameObject.hpp>
+
+// Define an enum for the card 
+enum class CardType {
+    Shotgun,
+    Shield
+};
+
+#include "ICardActivated.hpp"
+#include "ICardAvailability.hpp"
+#include "ICardVisible.hpp"
+
+class Card : public IGameObject
+{
+public:
+    Card() = default;
+    ~Card() override = default;
+    Card(const Card& other) = default;
+    Card& operator=(const Card& other) = default;
+    Card(Card&& other) noexcept = default;
+    Card& operator=(Card&& other) noexcept = default;
+
+    Card(CardType cardtype);
+    void Update() override;
+    void OnStart() override;
+
+    void PerformVisible(CardState cardstate) { if (m_visible) m_visible->Visible(this->m_textureName, cardstate, m_layerFloat, m_scaleFloat); }
+    void PerformAvailable() { if (m_available) m_available->Available(); }
+    void PerformActivated(DX::XMFLOAT2 position, DX::XMFLOAT2 direction) { if (m_activated) m_activated->Activate(position, direction); }
+
+    CardType GetType();
+private:
+    std::shared_ptr<ICardAvailability> m_available;
+    std::shared_ptr<ICardActivated> m_activated;
+    std::shared_ptr<ICardVisible> m_visible;
+
+    CardType m_type;
+};
