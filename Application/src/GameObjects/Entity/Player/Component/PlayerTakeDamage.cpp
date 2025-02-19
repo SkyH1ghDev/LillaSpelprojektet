a7317a4fce86db8af82ba897f4274b5edaa92943
LioTB0 <lio.ballantyne@gmail.com>
2025-02-19 11:19:50 +0100

    AAAAAAAAGH

--- Application/src/GameObjects/Entity/Player/Component/PlayerTakeDamage.cpp
+++ Application/src/GameObjects/Entity/Player/Component/PlayerTakeDamage.cpp
@@ -1,10 +1,18 @@
 #include "PlayerTakeDamage.hpp"
 #include "StatSheet.hpp"
+#include <SpEngine/Audio/Sound.hpp>
 
 void PlayerTakeDamage::TakeDamage(float& hp, float damage)
 {
     hp -= damage;
     HealthBarManager::RemoveHeart(damage);
+
+
+    if (hp <= 0)
+        Sound::Play("aaagh.wav", 0.5f);
+    else 
+        Sound::Play("ugh.wav", 0.5f);
+
 }
 
 void PlayerTakeDamage::SetHealth(float& hp)
