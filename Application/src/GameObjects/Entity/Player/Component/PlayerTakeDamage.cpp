05d09757015ae74c202099d1525a49883eb8f51b
Shoruda <123826451+Shoruda@users.noreply.github.com>
2025-02-19 12:58:45 +0100

    fix :)

--- Application/src/GameObjects/Entity/Player/Component/PlayerTakeDamage.cpp
+++ Application/src/GameObjects/Entity/Player/Component/PlayerTakeDamage.cpp
@@ -4,19 +4,19 @@
 
 void PlayerTakeDamage::TakeDamage(float& hp, float damage, bool& isActive, bool& shouldRender, float& iFrameTimer)
 {
-    hp -= damage;
-    HealthBarManager::RemoveHeart(damage);
-
     if (iFrameTimer <= 0)
     {
         hp -= damage;
         HealthBarManager::RemoveHeart(damage);
-        Sound::Play("ugh.wav", 0.5f);
         if (hp <= 0)
         {
             Sound::Play("aaagh.wav", 0.5f);
             isActive = false;
             shouldRender = false;
+        }
+        else
+        {
+            Sound::Play("ugh.wav", 0.5f);
         }
         iFrameTimer = 1.0;
     }
