b3dbdc54fadce445a8b326a32a249795d63c42d9
GuptaWaltersson <huwa22@student.bth.se>
2025-02-19 16:39:29 +0100

    animations

--- Application/src/GameObjects/Projectile/BishopBall/BishopBallVisible.cpp
+++ Application/src/GameObjects/Projectile/BishopBall/BishopBallVisible.cpp
@@ -3,8 +3,8 @@
 void BishopBallVisible::Visible(std::string& texture, DX::XMFLOAT2 position, ProjectileState entityState, float& layer, float& scale)
 {
     layer = 0.5 + 0.3 * position.y / 360;
-    scale = 0.05;
-    texture = "proj.png";
+    scale = 1.0;
+    texture = "fireball.apng";
 }
 
 void BishopBallVisible::UpdateLayer(DX::XMFLOAT2 position, float& layer)
