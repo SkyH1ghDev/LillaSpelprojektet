46f9dd7e1f2b9f2015bef1f2252865ece804780c
GuptaWaltersson <huwa22@student.bth.se>
2025-02-25 10:53:23 +0100

    lite wands

--- Application/src/GameObjects/Projectile/PawnPellet/PawnPelletVisible.cpp
+++ Application/src/GameObjects/Projectile/PawnPellet/PawnPelletVisible.cpp
@@ -3,8 +3,8 @@
 void PawnPelletVisible::Visible(std::string& texture, DX::XMFLOAT2 position, ProjectileState entityState, float& layer, float& scale)
 {
     layer = 0.5 + 0.3 * position.y / 360;
-    scale = 1.5;
-    texture = "pellet.apng";
+    scale = 1.0;
+    texture = "BlueBulletTwo.apng";
 }
 
 void PawnPelletVisible::UpdateLayer(DX::XMFLOAT2 position, float& layer)
