40ed7b3935427ca8ca4fcac363252ca96b110984
Ponlin02 <ponlin.02@gmail.com>
2025-02-28 12:22:07 +0100

    Fixed hitbox error

--- Application/src/GameObjects/Projectile/DisruptorWave/DisruptorWaveVisible.cpp
+++ Application/src/GameObjects/Projectile/DisruptorWave/DisruptorWaveVisible.cpp
@@ -1,6 +1,6 @@
 #include "DisruptorWaveVisible.hpp"
 
-void DisruptorWaveVisible::Visible(std::string& texture, DX::XMFLOAT2 position, ProjectileState entityState, float& layer, float& scale)
+void DisruptorWaveVisible::Visible(std::string& texture, DX::XMFLOAT2 position, ProjectileState entityState, float& layer, float& scale, bool& hasDeathAnimation)
 {
     layer = 0.5 + 0.3 * position.y / 360;
     scale = 1.0;
