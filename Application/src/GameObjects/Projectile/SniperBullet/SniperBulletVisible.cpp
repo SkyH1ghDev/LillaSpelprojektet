d758eb07616c465e446e4458f1ae24b63e88161b
Ponlin02 <ponlin.02@gmail.com>
2025-03-01 16:05:55 +0100

    Updated art
    Updated art for rune shard and added death animation

--- Application/src/GameObjects/Projectile/SniperBullet/SniperBulletVisible.cpp
+++ Application/src/GameObjects/Projectile/SniperBullet/SniperBulletVisible.cpp
@@ -2,9 +2,18 @@
 
 void SniperBulletVisible::Visible(std::string& texture, DX::XMFLOAT2 position, ProjectileState entityState, float& layer, float& scale, bool& hasDeathAnimation)
 {
+    hasDeathAnimation = true;
     layer = 0.5 + 0.3 * position.y / 360;
-    scale = 1.0;
-    texture = "sniper_bullet.apng";
+    scale = 1.2f;
+    switch (entityState)
+    {
+    case ProjectileState::Exploding:
+        texture = "rune_shard_death";
+        break;
+    default:
+        texture = "rune_shard";
+        break;
+    }
 }
 
 void SniperBulletVisible::UpdateLayer(DX::XMFLOAT2 position, float& layer)
