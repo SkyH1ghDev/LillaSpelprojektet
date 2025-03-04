05851759aad412dd4f92f07a6c3feebd9d0ab2b3
Shoruda <123826451+Shoruda@users.noreply.github.com>
2025-03-04 18:23:55 +0100

    fix

--- Application/src/Collision/CollisionHandler.cpp
+++ Application/src/Collision/CollisionHandler.cpp
@@ -128,7 +128,7 @@ void CollisionHandler::HandleCollision(const std::shared_ptr<IGameObject>& objA,
         auto player = std::dynamic_pointer_cast<Entity>(objA);
         auto enemy = std::dynamic_pointer_cast<Entity>(objB);
 
-        if (player && enemy)
+        if (player && enemy && enemy->GetState() != EntityState::Dying)
         {
             player->PerformTakeDamage(1);
 
@@ -165,7 +165,7 @@ void CollisionHandler::HandleCollision(const std::shared_ptr<IGameObject>& objA,
         auto player = std::dynamic_pointer_cast<Entity>(objB);
         auto enemy = std::dynamic_pointer_cast<Entity>(objA);
 
-        if (player && enemy)
+        if (player && enemy && enemy->GetState() != EntityState::Dying)
         {
             player->PerformTakeDamage(1);
 
