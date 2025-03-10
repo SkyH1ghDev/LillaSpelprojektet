188c6d934a38711b01d5373c9462b659036c39f8
GuptaWaltersson <huwa22@student.bth.se>
2025-03-10 17:03:41 +0100

    start hehe

---
+++ Application/src/Abilities/createEffect.cpp
@@ -0,0 +1,20 @@
+#include "AbilityManager.hpp"
+#include "ProjectileManager.hpp"
+
+
+/**
+  * Creates projectiles based on input parameters
+  * @param position Position to spawn projectiles
+  * @param direction Travel direction for projectiles
+  * @param projectileCount Number of projectiles to spawn
+  * @param lifeSpan Lifetime of projectile in seconds
+  * @param shotSpeed Speed of projectiles in pixels per second
+  * @param spreadAngle Angle offset between projectiles
+  */
+void AbilityManager::longEffect(DX::XMFLOAT2 position, DX::XMFLOAT2 direction, size_t projectileCount, float lifeSpan, float shotSpeed, float spreadAngle)
+{
+    // Convert direction to XMVECTOR for calculations
+
+}
+
+
